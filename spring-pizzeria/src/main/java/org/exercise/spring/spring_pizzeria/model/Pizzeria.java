796575f477c6a495a664fb47b537bab19ca5dfe2
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-04-03 10:06:41 +0200

    entity

---
+++ spring-pizzeria/src/main/java/org/exercise/spring/spring_pizzeria/model/Pizzeria.java
@@ -0,0 +1,85 @@
+package org.exercise.spring.spring_pizzeria.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "pizze")
+public class Pizzeria {
+
+    // variabili d'istanza(colonne tabella)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    private String nome;
+
+    @Lob
+    private String description;
+
+    private String foto;
+
+    @NotBlank
+    private BigDecimal prezzo;
+
+    // getter e setter
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFoto() {
+        return this.foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public BigDecimal getPrezzo() {
+        return this.prezzo;
+    }
+
+    public void setPrezzo(BigDecimal prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " id='" + getId() + "'" +
+                ", nome='" + getNome() + "'" +
+                ", description='" + getDescription() + "'" +
+                ", foto='" + getFoto() + "'" +
+                ", prezzo='" + getPrezzo() + "'" +
+                "}";
+    }
+
+}
